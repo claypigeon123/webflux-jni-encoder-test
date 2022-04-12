328677c69aee7b1ef43a3e394c1050c05cfb6601
claypigeon123 <n.zoltan18@gmail.com>
2022-04-12 17:42:49 +0200

    bootstrapped webflux app

---
+++ jni-test-java-app/src/main/java/com/cp/jnitest/java/jnitestjavaapp/JniTestJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.cp.jnitest.java.jnitestjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JniTestJavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JniTestJavaAppApplication.class, args);
+    }
+
+}
