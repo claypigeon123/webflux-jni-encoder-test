58f1628efd59b820495efafe853bdb7e82aceade
claypigeon123 <n.zoltan18@gmail.com>
2022-04-15 20:16:01 +0200

    changed to my own native base64 implementation

---
+++ jni-test-native/base64.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+
+namespace cp {
+	typedef unsigned char uchar;
+
+    struct encode_holder {
+        char* encoded_bytes;
+        size_t length;
+    };
+
+	static const std::string b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    // ---
+
+	static cp::encode_holder* base64_encode(char* plain_bytes, size_t plain_length) {
+        std::string plain_str(plain_bytes, plain_length);
+        std::string encoded_str;
+
+        int val = 0, valb = -6;
+        for (uchar c : plain_str) {
+            val = (val << 8) + c;
+            valb += 8;
+            while (valb >= 0) {
+                encoded_str.push_back(b[(val >> valb) & 0x3F]);
+                valb -= 6;
+            }
+        }
+        if (valb > -6) encoded_str.push_back(b[((val << 8) >> (valb + 8)) & 0x3F]);
+        while (encoded_str.size() % 4) encoded_str.push_back('=');
+
+        cp::encode_holder* encode_holder = new cp::encode_holder();
+        encode_holder->length = encoded_str.size();
+        encode_holder->encoded_bytes = new char[encode_holder->length];
+
+        for (int i = 0; i < encode_holder->length; i++) {
+            encode_holder->encoded_bytes[i] = encoded_str.at(i);
+        }
+
+        return encode_holder;
+	}
+
+    static void free_encode_holder(cp::encode_holder* encode_holder) {
+        delete[] encode_holder->encoded_bytes;
+        delete encode_holder;
+    }
+}
--- jni-test-native/com_cp_jnitest_java_jnitestjavaapp_jni_EncoderJNI.cpp
+++ jni-test-native/com_cp_jnitest_java_jnitestjavaapp_jni_EncoderJNI.cpp
@@ -1,58 +1,34 @@
 #include "com_cp_jnitest_java_jnitestjavaapp_jni_EncoderJNI.h"
 #include <string>
-#define CBASE64_IMPLEMENTATION
-#include "cbase64.h"
-
-namespace cp {
-	struct file_holder {
-		char* encoded_bytes;
-		size_t length;
-	};
-}
+#include "base64.h"
 
 JNIEXPORT jlong JNICALL Java_com_cp_jnitest_java_jnitestjavaapp_jni_EncoderJNI_new_1FileHolder(JNIEnv* jenv, jclass jcls, jbyteArray byte_array) {
 	jsize j_length = jenv->GetArrayLength(byte_array);
 	jbyte* j_bytes = jenv->GetByteArrayElements(byte_array, NULL);
 
-	const unsigned int encoded_length = cbase64_calc_encoded_length(j_length + 1);
-	char* encoded_out = (char*)malloc(encoded_length);
-	char* encoded_out_end = encoded_out;
+	cp::encode_holder* encode_holder = cp::base64_encode((char*)j_bytes, (size_t)j_length);
 
-	cbase64_encodestate encode_state;
-	cbase64_init_encodestate(&encode_state);
-	encoded_out_end += cbase64_encode_block((unsigned char*)j_bytes,(uint64_t) j_length + 1, encoded_out_end, &encode_state);
-	encoded_out_end += cbase64_encode_blockend(encoded_out_end, &encode_state);
-
-	jenv->ReleaseByteArrayElements(byte_array, j_bytes, JNI_ABORT);
-
-	size_t final_length = (encoded_out_end - encoded_out);
-
-	cp::file_holder* file_holder = new cp::file_holder();
-	file_holder->encoded_bytes = encoded_out;
-	file_holder->length = final_length;
-
-	return (jlong) file_holder;
+	return (jlong) encode_holder;
 }
 
 JNIEXPORT void JNICALL Java_com_cp_jnitest_java_jnitestjavaapp_jni_EncoderJNI_delete_1FileHolder(JNIEnv* jenv, jclass jcls, jlong ptr) {
-	cp::file_holder* file_holder = (cp::file_holder*) ptr;
+	cp::encode_holder* encode_holder = (cp::encode_holder*) ptr;
 
-	free(file_holder->encoded_bytes);
-	delete file_holder;
+	cp::free_encode_holder(encode_holder);
 }
 
 JNIEXPORT jlong JNICALL Java_com_cp_jnitest_java_jnitestjavaapp_jni_EncoderJNI_FileHolder_1encoded_1size(JNIEnv* jenv, jclass jcls, jlong ptr) {
-	cp::file_holder* file_holder = (cp::file_holder*)ptr;
+	cp::encode_holder* encode_holder = (cp::encode_holder*)ptr;
 
-	return file_holder->length;
+	return encode_holder->length;
 }
 
 JNIEXPORT void JNICALL Java_com_cp_jnitest_java_jnitestjavaapp_jni_EncoderJNI_FileHolder_1write(JNIEnv* jenv, jclass jcls, jlong ptr, jobject byte_buffer) {
-	cp::file_holder* file_holder = (cp::file_holder*)ptr;
+	cp::encode_holder* encode_holder = (cp::encode_holder*)ptr;
 
-	jbyteArray encoded_bytes_array = jenv->NewByteArray((jsize)file_holder->length);
+	jbyteArray encoded_bytes_array = jenv->NewByteArray((jsize)encode_holder->length);
 
-	jenv->SetByteArrayRegion(encoded_bytes_array, 0, jenv->GetArrayLength(encoded_bytes_array), (jbyte*)file_holder->encoded_bytes);
+	jenv->SetByteArrayRegion(encoded_bytes_array, 0, jenv->GetArrayLength(encoded_bytes_array), (jbyte*)encode_holder->encoded_bytes);
 
 	jbyte* buffer = (jbyte*) jenv->GetDirectBufferAddress(byte_buffer);
 	jenv->GetByteArrayRegion(encoded_bytes_array, 0, jenv->GetArrayLength(encoded_bytes_array), buffer);
